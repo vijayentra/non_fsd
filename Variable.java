4a9b38c2d10429c40ee8fe23bf210189134bfed8
vijayentra <115219980+vijayentra@users.noreply.github.com>
2023-05-03 10:47:24 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,20 @@
+
+public class Variable {
+
+	public static void main(String[] args) {
+		int x=10;
+		float y=20.54f;
+		char ch='6';
+		ch++;
+		String line = "the best";
+		boolean hi = true;
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(ch);
+		System.out.println(line);
+		System.out.println(hi);
+		
+	}
+
+}
